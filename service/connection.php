677d4f5b0aa4d430bc3dec5482884443e09c8000
denixxe <denissegarciaramirez@gmail.com>
2024-11-23 17:57:46 -0600

    jsjs

--- service/connection.php
+++ service/connection.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db_name = "crm_1";
+$servername = "bo7u6pimi9mondx2jxvm-mysql.services.clever-cloud.com";
+$username = "ujcxv1mcmvh3szov";
+$password = "HC2zESAuuPDUBO3WLngB";
+$db_name = "bo7u6pimi9mondx2jxvm";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$db_name);
