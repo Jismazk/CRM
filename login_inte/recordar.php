3912dab12070349078c072d129bfc3f27522e9aa
Jismazk <jismaelzk@outlook.es>
2024-11-26 07:39:08 -0600

    systema de mail configurado

--- login_inte/recordar.php
+++ login_inte/recordar.php
@@ -1,9 +1,40 @@
 <?php 
 include("../service/connection.php");
 
-if(empty($_POST)){
-    $email =filter_input(INPUT_POST, "correo", FILTER_SANITIZE_SPECIAL_CHARS);
-    
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer-master/PHPMailer-master/src/Exception.php';
+require '../PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
+require '../PHPMailer-master/PHPMailer-master/src/SMTP.php';
+
+if(!empty($_POST)){
+    $mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'crmutt450@gmail.com';                     //SMTP username
+    $mail->Password   = 'sisk qxtk joas ufek';                               //SMTP password
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('crmutt450@gmail.com', 'Mailer');
+    $mail->addAddress('jismaelzk@outlook.es', 'Joe User');     //Add a recipient
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
 }
 ?>
