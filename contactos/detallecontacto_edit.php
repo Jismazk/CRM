be10cc1c7377f39bd24187ed91918d3ff66ab45f
Jismazk <jismaelzk@outlook.es>
2024-12-01 08:35:15 -0600

    conexion a update contactos lista

--- contactos/detallecontacto_edit.php
+++ contactos/detallecontacto_edit.php
@@ -1,22 +1,22 @@
 <?php
 include("../service/connection.php");
-$sql = "SELECT  * FROM  contactos where id =";
-$result = mysqli_query($conn,$sql);
-if(!$result){
-    echo "problema con la conexion";
-}
-    if(!isset($_GET["id"])){
-        header("location: contactos.php");
+    if(isset($_GET["id"])){  
+        $d =  mysqli_real_escape_string($conn, $_GET['id']);
+        $sql = "SELECT  * FROM  contactos where id_contacto = '$d'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_assoc($result);
+  }else{
+    header("location: contactos.php");
         exit;
-    
-if($_SERVER["REQUEST_METHOD"] == $_GET){
-    if(!isset($_GET["id"])){
-        header("location: contactos/contactos.php");
-        exit;
-    }
-}
-}
+  }
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nom = filter_input(INPUT_POST,"contactoedit-n",FILTER_SANITIZE_SPECIAL_CHARS);
+    $up = "UPDATE contactos set Nombre = '$nom'";
+    $update = mysqli_query($conn,$up);
+  }
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,27 +30,29 @@ if($_SERVER["REQUEST_METHOD"] == $_GET){
 <body>
     <div class="detallesusuario">
         <h2>Detalles del contacto</h2>
+    <form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST"  >
     <label for="Empresa">Empresa</label>
-    <input type="text" id="Empresa" name="contacto-e">
+    <input type="text" id="Empresa" name="contactoedit-e" value="<?php echo $row["Empresa"] ?>">
 
     <label for="Nombre">Nombre</label>
-    <input type="text" id="Nombre" name="contacto-n">
+    <input type="text" id="Nombre" name="contactoedit-n" value="<?php echo $row["Nombre"] ?>">
 
     <label for="ApellidoP">Apellido</label>
-    <input type="text" id="ApellidoP" name="contacto-a">
+    <input type="text" id="ApellidoP" name="contactoedit-a" value="<?php echo $row["Apellido"] ?>">
 
     <label for="Telefono">Telefono</label>
-    <input type="tel"  id="Telefono" name="contacto-t">
+    <input type="tel"  id="Telefono" name="contactoedit-t" value="<?php echo $row["Numero"] ?>">
 
     <label for="Direccion">Direccion</label>
-    <input type="text" id="" name="contacto-d">
+    <input type="text" id="" name="contactoedit-d" value="<?php echo $row["Direccion"] ?>">
 
     <label for="Correo">Correo</label>
-    <input type="text" id="Correo" name="contacto-c">
+    <input type="text" id="Correo" name="contactoedit-c" value="<?php echo $row["Correo"] ?>">
 
     
     <br>
         <button id="Actualizar" type="submit" onclick="">Actualizar</button>
+    </form>
 
     </div>
   
